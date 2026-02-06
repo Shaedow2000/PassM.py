2734d5cd490b50eedc60ef535fb1c94e95b6eab3
Shaedow2000 <amsal8795@gmail.com>
2026-02-06 22:06:40 +0100

    Add command input for user.

--- src/app/main.py
+++ src/app/main.py
@@ -4,3 +4,8 @@ def main() -> None:
     menu: str = '\tg: generate passwd | c: clear screen | q: quit or [ Ctrl + C ]'
 
     print( menu )
+    while True:
+        command: str = input( '>=> ' ).replace( ' ', '' ).lower()
+
+if __name__ == '__main__':
+    main()
