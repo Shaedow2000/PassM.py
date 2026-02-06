fdebea0486c87ba5958f9a28bff93f9312e8ce02
Shaedow2000 <amsal8795@gmail.com>
2026-02-06 22:10:41 +0100

    Create clear function.

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import os, sys
+
+def clear() -> None:
+    os.system( 'cls' if os.name == 'nt' else 'clear' )
+
+    return
+
 def main() -> None:
     menu: str = '\tg: generate passwd | c: clear screen | q: quit or [ Ctrl + C ]'
-
+    
     print( menu )
+
     while True:
         command: str = input( '>=> ' ).replace( ' ', '' ).lower()
 
