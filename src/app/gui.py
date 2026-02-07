dfece2492eabf807c771eca5022076c1fba640da
Shaedow2000 <amsal8795@gmail.com>
2026-02-07 11:04:02 +0100

    Hide other menus when opening another menu.

--- src/app/gui.py
+++ src/app/gui.py
@@ -5,6 +5,15 @@ window: Tk = Tk()
 passwd_gen_menu: Frame = Frame( window )
 passwd_manager_menu: Frame = Frame( window )
 
+test = Entry( passwd_manager_menu )
+test.pack()
+
+def hide_menus() -> None:
+    passwd_gen_menu.pack_forget()
+    passwd_manager_menu.pack_forget()
+
+    return
+
 def gui() -> None:
     window.geometry( '950x800' )
     window.title( 'PassM' )
@@ -19,10 +28,10 @@ def gui() -> None:
     buttons_side.pack( fill='y', side='left' )
 
     # buttons
-    passwd_gen_button: Button = Button( buttons_side, text='Password Generation', font=( 'Impact', 16, 'bold' ), width=25, command=lambda: passwd_gen_menu.pack() )
+    passwd_gen_button: Button = Button( buttons_side, text='Password Generation', font=( 'Impact', 16, 'bold' ), width=25, command=lambda: ( hide_menus(), passwd_gen_menu.pack() ) )
     passwd_gen_button.pack()
 
-    passwd_manager_button: Button = Button( buttons_side, text='Password Manager', font=( 'Imapct', 16, 'bold' ), width=25, command=lambda: passwd_manager_menu.pack() )
+    passwd_manager_button: Button = Button( buttons_side, text='Password Manager', font=( 'Imapct', 16, 'bold' ), width=25, command=lambda: ( hide_menus(), passwd_manager_menu.pack() ) )
     passwd_manager_button.pack()
 
     print( '=> Opened GUI...' )
