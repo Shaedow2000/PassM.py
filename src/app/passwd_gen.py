ac9236fec922ab359931afc7f47cc4acc29a43b8
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:08 +0000

    Add use_only param to gen passwd function.

--- src/app/passwd_gen.py
+++ src/app/passwd_gen.py
@@ -2,7 +2,18 @@ import string, random
 
 chars: list[ str ] = [ *string.ascii_letters, *string.digits, *string.punctuation ]
 
-def gen_passwd( lenght: int ) -> str:
+def gen_passwd( lenght: int, use_only: str ) -> str:
+    global chars
+
+    if use_only == 'letters':
+        chars = [ *string.ascii_letters ]
+    elif use_only == 'digits':
+        chars = [ *string.digits ]
+    elif use_only == 'punctuation':
+        chars = [ *string.punctuation ]
+    else:
+        chars = chars
+
     passwd: str = ''
     index: int = random.randint( 0, len( chars ) - 1 )
 
