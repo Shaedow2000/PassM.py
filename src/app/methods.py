f68685e21e854360d49cb20ddfec9ac3aca51684
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:10 +0000

    Comment out the unused clear function.

--- src/app/methods.py
+++ src/app/methods.py
@@ -3,23 +3,23 @@ from tkinter import END, Entry
 
 menu: str = '\tg: generate passwd | c: clear screen | q: quit or [ Ctrl + C ]'
 
-def clear() -> None:
-    os.system( 'cls' if os.name == 'nt' else 'clear' )
-
-    print( menu )
-
-    return
-
-def quit() -> None:
-    sys.exit( '\n!> Quiting program...' )
-
-def is_int( n: str ) -> bool:
-    try:
-        int( n )
-
-        return True
-    except ValueError:
-        return False
+# def clear() -> None:
+#     os.system( 'cls' if os.name == 'nt' else 'clear' )
+#
+#     print( menu )
+#
+#     return
+#
+# def quit() -> None:
+#     sys.exit( '\n!> Quiting program...' )
+#
+# def is_int( n: str ) -> bool:
+#     try:
+#         int( n )
+#
+#         return True
+#     except ValueError:
+#         return False
 
 # data file path
 dir: str = os.path.dirname( __file__ )
