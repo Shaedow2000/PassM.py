55ff009c5da938951083c033f5dee5459e0de3f5
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:08 +0000

    Add menus var.

--- src/app/main.py
+++ src/app/main.py
@@ -3,34 +3,39 @@
 import pyperclip
 
 from methods import clear, quit, is_int
-from passwd_gen import gen_passwd
+from passwd_gen import gen_passwd 
+from gui import gui
 
 def main() -> None:
-    clear()
+    #### CLI version [ old ]
+    # clear()
+    #
+    # while True:
+    #     command: str = input( '>=> ' ).replace( ' ', '' ).lower()
+    #
+    #     if command == 'g':
+    #         lenght: str = input( '--> Enter lenght of the password [ Enter => 16 ]: ' ).replace( ' ', '' )
+    #         print( '- password contains: [ press any key ]> All chars | 1> letters only | 2> digits only | 3> punctuation only' )
+    #         use_only: str = input( '--> ' ).replace( ' ', '' )
+    #
+    #         if is_int( lenght ) or lenght == '':
+    #             passwd: str = gen_passwd( int( lenght ) if lenght != '' else 16, use_only )
+    #             print( f'!> [  { passwd }  ]' )
+    #             pyperclip.copy( passwd )
+    #             print( '|=> Password copied to clipboard !' )
+    #         else:
+    #             print( f'!> Value { lenght } is not a number.' )
+    #     elif command == 'c':
+    #         clear()
+    #     elif command == 'q':
+    #         quit()
+    #     elif command == '':
+    #         pass
+    #     else:
+    #         print( f'!> Command not found: [ { command } ].' )
 
-    while True:
-        command: str = input( '>=> ' ).replace( ' ', '' ).lower()
+    gui()
 
-        if command == 'g':
-            lenght: str = input( '--> Enter lenght of the password [ Enter => 16 ]: ' ).replace( ' ', '' )
-            print( '- password contains: [ press any key ]> All chars | 1> letters only | 2> digits only | 3> punctuation only' )
-            use_only: str = input( '--> ' ).replace( ' ', '' )
-
-            if is_int( lenght ) or lenght == '':
-                passwd: str = gen_passwd( int( lenght ) if lenght != '' else 16, use_only )
-                print( f'!> [  { passwd }  ]' )
-                pyperclip.copy( passwd )
-                print( '|=> Password copied to clipboard !' )
-            else:
-                print( f'!> Value { lenght } is not a number.' )
-        elif command == 'c':
-            clear()
-        elif command == 'q':
-            quit()
-        elif command == '':
-            pass
-        else:
-            print( f'!> Command not found: [ { command } ].' )
 
 if __name__ == '__main__':
     try:
