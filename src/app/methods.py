c824d5c660ff578009631d862d9e5a6f90bf7248
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:09 +0000

    Create function to read json file.

--- src/app/methods.py
+++ src/app/methods.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, json
 
 menu: str = '\tg: generate passwd | c: clear screen | q: quit or [ Ctrl + C ]'
 
@@ -19,3 +19,20 @@ def is_int( n: str ) -> bool:
         return True
     except ValueError:
         return False
+
+def read_json() -> str:
+    dir: str = os.path.dirname( __file__ )
+    file_name: str = 'data.json'
+
+    file_path: str = os.path.join( dir, 'assets', file_name )
+
+    if not os.path.exists( file_path ):
+        with open( file_path, 'w' ) as file:
+            json.dump( { "key": "", "decKey": "", "data": "" }, file )
+
+    data: str = ''
+
+    with open( file_path, 'r' ) as file:
+        data = json.load( file )
+
+    return data
