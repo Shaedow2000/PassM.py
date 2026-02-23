94d940f7a9a1aae2a1117d9fe85930224b3c29a9
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:09 +0000

    Create function to write json file.

--- src/app/methods.py
+++ src/app/methods.py
@@ -20,12 +20,12 @@ def is_int( n: str ) -> bool:
     except ValueError:
         return False
 
+# data file path
+dir: str = os.path.dirname( __file__ )
+file_name: str = 'data.json'
+file_path: str = os.path.join( dir, 'assets', file_name )
+
 def read_json() -> str:
-    dir: str = os.path.dirname( __file__ )
-    file_name: str = 'data.json'
-
-    file_path: str = os.path.join( dir, 'assets', file_name )
-
     if not os.path.exists( file_path ):
         with open( file_path, 'w' ) as file:
             json.dump( { "key": "", "decKey": "", "data": "" }, file )
@@ -36,3 +36,9 @@ def read_json() -> str:
         data = json.load( file )
 
     return data
+
+def write_json( data: str ) -> None:
+    with open( file_path, 'w' ) as file:
+        json.dump( data, file )
+
+    return
