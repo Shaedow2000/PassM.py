5a6a093bf6b5361032555ef2f249ef1c69e94132
Shaedow2000 <shaedow2000@proton.me>
2026-02-23 22:07:09 +0000

    Add function to save key & Create login menu.

--- src/app/methods.py
+++ src/app/methods.py
@@ -25,20 +25,27 @@ dir: str = os.path.dirname( __file__ )
 file_name: str = 'data.json'
 file_path: str = os.path.join( dir, 'assets', file_name )
 
-def read_json() -> str:
+def read_json() -> dict:
     if not os.path.exists( file_path ):
         with open( file_path, 'w' ) as file:
             json.dump( { "key": "", "decKey": "", "data": "" }, file )
 
-    data: str = ''
+    data: dict = {}
 
     with open( file_path, 'r' ) as file:
         data = json.load( file )
 
     return data
 
-def write_json( data: str ) -> None:
+def write_json( data: dict ) -> None:
     with open( file_path, 'w' ) as file:
         json.dump( data, file )
 
     return
+
+def write_passkey( key: str ) -> None:
+    data: dict = read_json()
+
+    data[ 'key' ] = key
+
+    write_json( data )
